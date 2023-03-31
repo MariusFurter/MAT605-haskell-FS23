76bfb8984fc3c393b4161376a22313f371c2dd4d
MariusFurter <marius.furter@gmail.com>
2023-03-31 15:48:15 +0200

    relations

--- scripts/Haskell 7 :: Implementing Relations/relations.hs
+++ scripts/Haskell 7 :: Implementing Relations/relations.hs
@@ -1,43 +1,50 @@
 import SetList
 
-type Rel a = Set (a,a)
+-- Relations R: A -> A
+type Rel a = Set (a,a) 
+-- Relations R: A -> B
+type Rel' a b = Set (a,b) 
+-- Relation R: A -> A as function
+type FunRel a = a -> a -> Bool 
+-- Relations R: A -> B as functions
+type FunRel' a b = a -> b -> Bool
 
-type Rel' a = a -> a -> Bool
-
+-- Domain / range of a relation
 domRel :: Eq a => Rel a -> Set a
-domRel r = toSet [x | (x,_) <- unSet r]
+domRel r = toSet [fst xs | xs <- unSet r ] 
+
+domRel' :: Eq a => Rel a -> Set a
+domRel' r = toSet [x | (x,_) <- unSet r ] 
 
 ranRel :: Eq a => Rel a -> Set a
-ranRel r = toSet [y | (_,y) <- unSet r]
+ranRel r = toSet [snd xs | xs <- unSet r ] 
 
-invRel :: Eq a => Rel a -> Rel a
-invRel r = toSet [(y,x) | (x,y) <- unSet r]
+ranRel' :: Eq a => Rel a -> Set a
+ranRel' r = toSet [y | (_,y) <- unSet r ] 
 
-invRel' :: Eq a => Rel' a -> Rel' a
-invRel' r = flip r
+-- Check if (x,y) is in relation
+inRel :: Eq a => (a,a) -> Rel a -> Bool 
+inRel xs r = xs `inSet` r 
 
-inRel :: Eq a => (a, a) -> Rel a -> Bool
-inRel = inSet
+inRel' :: Eq a => (a,a) -> Rel a -> Bool 
+inRel' = inSet 
 
-inRel' :: Eq a => a -> a -> Rel' a -> Bool
-inRel' x y r = r x y
+-- Properties:
+-- Reflexivity
+reflRel :: Eq a => Set a -> Rel a -> Bool 
+reflRel a r = and [(x,x) `inRel` r | x <- unSet a ] 
 
-reflRel :: Eq a => Set a -> Rel a -> Bool
-reflRel s r = and [(x,x) `inRel` r | x <- unSet s]
-
-reflRel' :: Eq a => Set a -> Rel' a -> Bool
-reflRel' s r = and [r x x | x <- unSet s]
-
+-- Symmetry
 symRel :: Eq a => Rel a -> Bool
-symRel r = and [(y,x) `inRel` r | (x,y) <- unSet r]
+symRel r = and [ (y,x) `inRel` r | (x,y) <- unSet r] 
 
-transRel :: Eq a => Rel a -> Bool
-transRel r = and [(x,y') `inRel` r | (x,y) <- unSet r, (x',y') <- unSet r, y == x']
+-- Transitivity
+transRel :: Eq a => Rel a -> Bool 
+transRel r = and [ (x,z') `inRel` r | 
+                        (x,y) <- unSet r, (y',z') <- unSet r, y == y' ]
 
+-- Relation composition
 infix 5 @@
 
-(@@) :: Eq a => Rel a -> Rel a -> Rel a
-r @@ s = toSet [(x,z) | (x,y) <- unSet r , (y',z) <- unSet s, y == y']
-
-compRel' :: Eq a => Set a -> Rel' a -> Rel' a -> Rel' a
-compRel' a r s x y = or [ r x u && s u y  | u <- unSet a ]
+(@@) :: Eq a => Rel a -> Rel a -> Rel a 
+r @@ s = toSet [(x,z') | (x,y) <- unSet r, (y',z') <- unSet s, y == y']
