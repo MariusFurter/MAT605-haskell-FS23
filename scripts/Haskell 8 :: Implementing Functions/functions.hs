424167781aad03a73e9671457f7ca48a380159a3
MariusFurter <marius.furter@gmail.com>
2023-04-10 10:54:34 +0200

    functions

--- scripts/Haskell 8 :: Implementing Functions/functions.hs
+++ scripts/Haskell 8 :: Implementing Functions/functions.hs
@@ -1,50 +1,53 @@
 import SetList
 import Data.List (nub)
 
--- Datatype for functions
-data Fun' a b = Fun' (Set a) (Set b) (Set (a,b)) deriving Show
+-- Datatype for function
+data Fun' a b = Fun' (Set a) (Set b) (Set (a,b)) deriving Show 
 
-data Fun a b = Fun {dom :: Set a, 
-                    cod :: Set b, 
-                    pairs :: Set (a, b)} deriving Show
+dom' :: Fun' a b -> Set a
+dom' (Fun' d c p) = d
 
-f = Fun (Set [1,2,3]) (Set ['a','b','c']) (Set [(1,'a'),(2,'b'), (3,'b')])
-g = Fun (Set [1,2,3]) (Set ['a','b','c']) (Set [(1,'a'),(2,'b'), (3,'c')])
+data Fun a b = Fun { dom :: Set a, 
+                     cod :: Set b, 
+                     pairs :: Set (a,b) } 
+                        deriving Show
 
-h = Fun (Set [1,2,3]) (Set ['a','b','c']) (Set [(2,'a'),(2,'b'), (3,'c')])
-
-numUnique ::Eq a => [a] -> Int
-numUnique = length . nub
-
+-- Check if Fun is actually a function
 getPairs :: Fun a b -> [(a,b)]
-getPairs f = unSet $ pairs f
+getPairs f = unSet (pairs f) 
 
--- Domain of definition
-domDef :: Eq a => Fun a b -> Set a
-domDef f = toSet $ map fst (getPairs f)
+numUnique :: Eq a => [a] -> Int
+numUnique = length . nub 
 
--- Image
+numYs :: (Eq a, Eq b) => Fun a b -> a -> Int 
+numYs f x = numUnique [y' | (x',y') <- getPairs f, x' == x ]
+
+isFun :: (Eq a, Eq b) => Fun a b -> Bool 
+isFun f = imFun f `subSet` cod f && 
+            and [ numYs f x == 1| x <- unSet (dom f) ]
+
+
+-- Get the image of a function
 imFun :: Eq b => Fun a b -> Set b
-imFun f = toSet $ map snd (getPairs f)
+imFun f = toSet [y | (x,y) <- getPairs f] 
 
--- Check if function
-isFun :: (Eq a, Eq b) => Fun a b -> Bool
-isFun f = imFun f `subSet` cod f && and [ numUnique [y' | (x',y') <- getPairs f, x' == x] == 1 | x <- unSet $ dom f]
-
--- Apply function
-applyFun :: Fun a b -> a -> b
-applyFun f a = head [y | (x,y) <- getPairs f]
-
--- Injective / Surjective
-isInj :: (Eq a, Eq b) => Fun a b -> Bool
-isInj f = and[ numUnique (nub $ [x' | (x',y') <- getPairs f, y' == y]) == 1 | y <- unSet $ cod f ]
-
-isSurj :: Eq b => Fun a b -> Bool
+-- Check if function is injective / surjective
+isSurj :: (Eq b) => Fun a b -> Bool
 isSurj f = imFun f == cod f
+
+numXs :: (Eq a, Eq b) => Fun a b -> b -> Int
+numXs f y = numUnique [x' | (x',y') <- getPairs f, y' == y ]
+
+isInj :: (Eq a, Eq b) => Fun a b -> Bool
+isInj f = and [ numXs f y <= 1 | y <- unSet (cod f) ]
 
 -- Conversion
 toFun :: (Eq a, Eq b) => (a -> b) -> [a] -> [b] -> Fun a b
-toFun f s t = Fun (toSet s) (toSet t) (toSet [(x,f x) | x <- s]) 
+toFun f s t = Fun (toSet s) (toSet t) (toSet [ (x, f x) | x <- s ])
 
-fromFun :: Eq a => Fun a b -> (a -> b) 
-fromFun f a = head [y | (x,y) <- getPairs f, x == a]
+fromFun :: (Eq a) => Fun a b -> (a -> b)
+fromFun f x = head [y' | (x',y') <- getPairs f, x' == x ]
+
+f = Fun (Set [1,2]) (Set ['a','b']) (Set [(1,'a'),(2,'b')])
+f' = fromFun f 
+g = Fun (Set [1,2]) (Set ['a','b']) (Set [(1,'a'),(2,'a')])
