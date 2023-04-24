d45f02950f507bef07dcc744836a0cca1555eaf2
MariusFurter <marius.furter@gmail.com>
2023-04-24 19:17:18 +0200

    induction_nat

--- scripts/Haskell 9 :: Induction and Recursion I/induction_nat.hs
+++ scripts/Haskell 9 :: Induction and Recursion I/induction_nat.hs
@@ -1,43 +1,54 @@
-data Nat = Z | S Nat deriving (Eq, Show)
+data Nat = Z | S Nat deriving (Show, Eq) 
 
+-- Conversion
 fromNat :: Nat -> Int
 fromNat Z = 0
 fromNat (S n) = fromNat n + 1 
 
 toNat :: Int -> Nat
 toNat 0 = Z
-toNat x | x > 0 = S (toNat (x-1))
-        | otherwise = error "negative number"
+toNat n | n > 0 = S ( toNat (n-1) )
+        | otherwise = error "number is negative"
 
+-- Arithmetic (+,*,^)
 plus :: Nat -> Nat -> Nat
 plus n Z = n
-plus n (S m) = S (plus n m)
+plus n (S m) = S ( plus n m )       -- n + (m+1) = (n+m) + 1 
 
 mult :: Nat -> Nat -> Nat
-mult n Z = Z
-mult n (S m) = n `plus` mult n m
+mult n Z = Z 
+mult n (S m) = (mult n m) `plus` n  -- n * (m+1) = (n*m) + n
 
 pow :: Nat -> Nat -> Nat
 pow n Z = S Z
-pow n (S m) = n `mult` pow n m
+pow n (S m) = (pow n m) `mult` n   -- n^(m+1) = (n^m) * n
 
+-- Comparison
 leq :: Nat -> Nat -> Bool
 leq Z _ = True
-leq (S _) Z = False
-leq (S n) (S m) = leq n m
+leq (S _) Z = False 
+leq (S n) (S m) = leq n m 
 
-qeg :: Nat -> Nat -> Bool
-qeg n m = leq m n
+geq :: Nat -> Nat -> Bool
+geq n m = leq m n
 
+lt :: Nat -> Nat -> Bool
+lt n m = not (geq n m) 
+
+gt :: Nat -> Nat -> Bool
+gt n m = lt m n 
+
+-- Folds over Nat 
+-- foldn h c N = h ( h ( ... h( c ))) 
 foldn :: (a -> a) -> a -> Nat -> a
 foldn h c Z = c
-foldn h c (S n) = h (foldn h c n)
+foldn h c (S n) = h ( foldn h c n )
 
 plus' :: Nat -> Nat -> Nat
-plus' n m = foldn S n m
+plus' = foldn S 
 
 mult' :: Nat -> Nat -> Nat
-mult' n m = foldn (plus' n) Z m
+mult' n = foldn (plus' n) Z
 
-expn' :: Nat -> Nat -> Nat
-expn' n m = foldn (mult' n) (S Z) m
+pow' :: Nat -> Nat -> Nat
+pow' n = foldn (mult' n) (S Z)
